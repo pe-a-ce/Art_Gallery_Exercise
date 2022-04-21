f5ec137ee5e1b1465f36f3904f187cc808b9d20b
Peace Akib <99202770+pe-a-ce@users.noreply.github.com>
2022-04-21 15:00:54 +0100

    Initial Upload

---
+++ src/main/java/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+
+    /*
+ + ----------------------- +
+         |        Book            |
+            + ------------------------+
+            |      - - name: String   |
+            |  - author: String       |
+            |      - pages: int       |
+            |      - inStock: int     |
+            |      - price: double    |
+            + ----------------------- +
+            | + to String(): String   |
+            | + isInStock(): boolean  |
+            | + getName(): String     |
+            | + getAuthor(): String   |
+            | + getPages(): int       |
+            | + getPrice(): double    |
+            + ----------------------- +
+
+    */
+
+}
